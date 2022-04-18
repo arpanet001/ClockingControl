48f2f02435c299fa6f3fec23c7d1bf941568a6de
Humphryshikunzi <humphryshikunzi9@gmail.com>
2022-04-18 17:43:13 +0300

    A Database Table Can not have duplicate primary keys

--- backend/Data/Migrations/20220418143441_TimeidMigration.Designer.cs
+++ backend/Data/Migrations/20220418143441_TimeidMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace clockingapi.Data.Migrations
 {
     [DbContext(typeof(clockingdbcontext))]
-    partial class clockingdbcontextModelSnapshot : ModelSnapshot
+    [Migration("20220418143441_TimeidMigration")]
+    partial class TimeidMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/Data/Migrations/20220418143441_TimeidMigration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace clockingapi.Data.Migrations
+{
+    public partial class TimeidMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TimeDetails",
+                table: "TimeDetails");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TimeId",
+                table: "TimeDetails",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TimeDetails",
+                table: "TimeDetails",
+                column: "TimeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TimeDetails",
+                table: "TimeDetails");
+
+            migrationBuilder.DropColumn(
+                name: "TimeId",
+                table: "TimeDetails");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TimeDetails",
+                table: "TimeDetails",
+                column: "PersonalFileNumber");
+        }
+    }
+}
